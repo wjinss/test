8b54cc142b7dbf0f3fdf4830ca351da9ba97a8db
우진 <wjinss@wjinssui-MacBookPro.local>
2024-10-16 14:19:59 +0900

    Prepare for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/movie/:id" element={<Detail/>}></Route>
-        <Route path="/" element={<Home/>}></Route>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={< Home />}></Route>
       </Routes>
     </Router>
   )
